b32e28891ab3d02da6e87c70744a446f576c44c8
Kensei Honda <kensei-honda@c-fo.com>
2018-02-12 16:58:02 +0900

    company

--- init.el
+++ init.el
@@ -10,12 +10,20 @@
 
 (require 'init-loader)
 (init-loader-load)
+
+;;company
 (require 'company)
 (global-company-mode)
 (setq company-idle-delay 0)
 (setq company-minimum-prefix-length 2)
 (setq company-selection-wrap^around t)
-;;(global-set-key [f8] 'neotree-toggle)
+(define-key company-active-map (kbd "M-n") nil)
+(define-key company-active-map (kbd "M-p") nil)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "C-h") nil)
+
+;;neotree
 (global-set-key "\C-q" 'neotree-toggle)
 ;; neotree ウィンドウを表示する毎に current file のあるディレクトリを表示する
 (setq neo-smart-open t)
